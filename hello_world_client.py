4e4524d062d5d148beeb7c6d6186e3ec0cf73bf0
John Maldonado <jmaldonado@contdisc.com>
2020-03-13 14:01:55 -0500

    Added comments to hello_world_client.py

--- hello_world_client.py
+++ hello_world_client.py
@@ -4,8 +4,11 @@ import socket
 server_address = input('Server IP Address: ')
 server_port = int(input('Server Port: '))
 
+# Create socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Connect to server
 s.connect((server_address, server_port))
-
+# Recieve message from server
 msg = s.recv(1024)
+# Decode and print message
 print(msg.decode("utf-8"))
